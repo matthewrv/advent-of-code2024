5646e5474d8f3c7c8ba3ec380dbcdb72c3194180
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2024-12-02 10:44:37 +0300

    Add solution for second puzzle of second day

--- 02_ab/main.go
+++ 02_ab/main.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+// change this constant to switch between first and second part
+const withDampener bool = true
 
 func main() {
 	reports := readFile("input.txt")
@@ -47,12 +51,21 @@ func readFile(fileName string) (reports [][]int) {
 
 func countSafeReports(reports [][]int) (count int) {
 	for _, report := range reports {
+		// fmt.Printf("Processing report %d\n", idx)  // for debug purpose only
 		count += isSafeReport(report)
 	}
 	return count
 }
 
 func isSafeReport(report []int) int { // return 1 if safe and 0 if not
+	if withDampener {
+		return isSafeWithDampener(report)
+	}
+	isSafe, _ := isSafeDefault(report)
+	return isSafe
+}
+
+func isSafeDefault(report []int) (int, int) { // return 1 if safe and 0 if not
 	var isAscending int = 1
 	if report[1]-report[0] < 0 {
 		isAscending = -1
@@ -60,8 +73,34 @@ func isSafeReport(report []int) int { // return 1 if safe and 0 if not
 	for idx, value := range report[:len(report)-1] {
 		diff := (report[idx+1] - value) * isAscending
 		if diff > 3 || diff < 1 {
-			return 0
+			// fmt.Printf("Failed on values %d, %d\n", value, report[idx+1])  // for debug purpose only
+			return 0, idx
 		}
 	}
-	return 1
+	// fmt.Println("Success")  // for debug purpose only
+	return 1, 0
+}
+
+func isSafeWithDampener(report []int) int { // return 1 if safe and 0 if not
+	isSafe, problemIdx := isSafeDefault(report)
+	if isSafe == 1 {
+		return isSafe
+	}
+
+	if problemIdx == 1 {
+		isSafe, _ = isSafeDefault(report[1:])
+		if isSafe == 1 {
+			return isSafe
+		}
+	}
+
+	testReport1 := slices.Concat(report[:problemIdx], report[problemIdx+1:])
+	isSafe, _ = isSafeDefault(testReport1)
+	if isSafe == 1 {
+		return isSafe
+	}
+
+	testReport2 := slices.Concat(report[:problemIdx+1], report[problemIdx+2:])
+	isSafe, _ = isSafeDefault(testReport2)
+	return isSafe
 }
