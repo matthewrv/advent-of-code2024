f03a6a1893f3c7d6543d21e327d12b7220405f12
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2024-12-09 10:29:28 +0300

    solve second part of day 8

--- 08_ab/main.go
+++ 08_ab/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	nodesLocation, mapSize := readInput("input_test.txt")
+	nodesLocation, mapSize := readInput("input.txt")
 	antinodesCount := countAntinodes(nodesLocation, mapSize)
 	fmt.Printf("Result: %d\n", antinodesCount)
 }
@@ -64,7 +64,7 @@ func countAntinodes(result map[byte][]location, mapSize [2]int) int {
 	for _, locations := range result {
 		for idx, loc1 := range locations {
 			for _, loc2 := range locations[idx+1:] {
-				antinodes := getAntinodesOf2(loc1, loc2, mapSize)
+				antinodes := getAntinodesOf2V2(loc1, loc2, mapSize)
 				for _, antinode := range antinodes {
 					uniqueAntinodes[antinode] = true
 				}
@@ -72,7 +72,6 @@ func countAntinodes(result map[byte][]location, mapSize [2]int) int {
 		}
 	}
 
-	// fmt.Printf("Unique antinodes: %v\n", uniqueAntinodes)
 	return len(uniqueAntinodes)
 }
 
@@ -92,6 +91,48 @@ func getAntinodesOf2(loc1 location, loc2 location, mapSize [2]int) (result []loc
 	return result
 }
 
+func getAntinodesOf2V2(loc1 location, loc2 location, mapSize [2]int) (result []location) {
+	diff := loc2.Sub(&loc1)
+	gcd := GCDEuclidean(diff.i, diff.j)
+	step := location{diff.i / gcd, diff.j / gcd}
+	// fmt.Printf("%v and %v: diff %v, GCD %d, step %v\n", loc1, loc2, diff, gcd, step)
+
+	current := loc1
+	for current.inMap(&mapSize) {
+		result = append(result, current)
+		current = current.Sub(&step)
+	}
+
+	current = loc1.Sum(&step)
+	for current.inMap(&mapSize) {
+		result = append(result, current)
+		current = current.Sum(&step)
+	}
+
+	// fmt.Printf("Antinodes are: %v\n", result)
+
+	return result
+}
+
 func (loc *location) inMap(mapSize *[2]int) bool {
 	return loc.i >= 0 && loc.j >= 0 && loc.i < mapSize[0] && loc.j < mapSize[1]
+}
+
+// GCDEuclidean calculates GCD by Euclidian algorithm.
+func GCDEuclidean(a, b int) int {
+	if a < 0 {
+		a = -a
+	}
+	if b < 0 {
+		b = -b
+	}
+	for a != b {
+		if a > b {
+			a -= b
+		} else {
+			b -= a
+		}
+	}
+
+	return a
 }
