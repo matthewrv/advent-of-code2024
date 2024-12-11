0f26590c67befa95fdc187921737f402ad1877df
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2024-12-11 09:39:46 +0300

    solve second part of day 9

--- 09_ab/main.go
+++ 09_ab/main.go
@@ -9,10 +9,10 @@ import (
 func main() {
 	space := readInput("input.txt")
 
-	// fmt.Print("Initial space: ")
+	// fmt.Print("Initial space:      ")
 	// printSpace(space)
 
-	result := defragment(space)
+	result := defragmentPart2(space)
 
 	// fmt.Print("Defragmented space: ")
 	// printSpace(space)
@@ -20,7 +20,7 @@ func main() {
 	fmt.Println("Result:", result)
 }
 
-// part 1
+// common
 
 func readInput(fileName string) (space []*int) {
 	content, _ := os.ReadFile(fileName)
@@ -43,6 +43,19 @@ func readInput(fileName string) (space []*int) {
 
 	return space
 }
+
+func printSpace(space []*int) {
+	for _, ref := range space {
+		if ref != nil {
+			fmt.Print(*ref, " ")
+		} else {
+			fmt.Print(".", " ")
+		}
+	}
+	fmt.Println()
+}
+
+// part 1
 
 func defragment(space []*int) (checksum int) {
 	l, r := 0, len(space)-1
@@ -67,13 +80,57 @@ func defragment(space []*int) (checksum int) {
 	return checksum
 }
 
-func printSpace(space []*int) {
-	for _, ref := range space {
-		if ref != nil {
-			fmt.Print(*ref, " ")
-		} else {
-			fmt.Print(".", " ")
+// part 2
+
+func defragmentPart2(space []*int) (checksum int) {
+	r := len(space) - 1
+	for r > 0 {
+		// find file block
+		for space[r] == nil {
+			r--
+		}
+
+		size := 0
+		fileId := space[r]
+		for r-size > 0 && space[r-size] == fileId {
+			size++
+		}
+
+		// find free space
+		l, available := 0, 0
+		for l < r && available != size {
+			// find start of free space
+			for space[l] != nil {
+				l++
+			}
+
+			// check if size is enough
+			for available != size && l+available < len(space) && space[l+available] == nil {
+				available++
+			}
+
+			if available != size {
+				l = l + available
+				available = 0
+			}
+		}
+
+		if l < r && available == size {
+			// swap
+			for i := 0; i < size; i++ {
+				space[l+i] = fileId
+				space[r-i] = nil
+			}
+		}
+		r = r - size
+	}
+
+	// calc checksum
+	for idx, blockId := range space {
+		if blockId != nil {
+			checksum += idx * *blockId
 		}
 	}
-	fmt.Println()
+
+	return checksum
 }
