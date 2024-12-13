07e5b3eb21a8dd9eb6e76e7d3093730621832690
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2024-12-13 17:36:24 +0300

    solve part 2 of day 11

--- 11/main.go
+++ 11/main.go
@@ -5,68 +5,75 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"slices"
 	"strconv"
 )
 
 func main() {
-	input := readInput("input.txt")
-	fmt.Println(input)
-	iters := 25
-	for iter := range iters {
-		input = transform(input)
-		fmt.Println("Finished iteration:", iter, "Current length:", len(input))
+	stones := readInput("input.txt")
+	fmt.Println(stones)
+	iters := 75
+	for range iters {
+		stones = blink(stones)
+		// fmt.Println("Finished iteration:", iter, "Current length:", len(stones))
 		// fmt.Println(input)
 	}
 
-	fmt.Println("Result: ", len(input))
+	var totalStones int
+	for _, count := range stones {
+		totalStones += count
+	}
+	fmt.Println("Result: ", totalStones)
 }
 
-func readInput(fileName string) (arr []int) {
+func readInput(fileName string) (stones map[int]int) {
 	content, err := os.ReadFile(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	stones = make(map[int]int)
 	for _, token := range bytes.Split(content, []byte(" ")) {
 		val, err := strconv.Atoi(string(token))
 		if err != nil {
 			log.Fatal(err)
 		}
-		arr = append(arr, val)
+		stones[val] += 1
 	}
 
-	return arr
+	return stones
 }
 
-func transform(input []int) (newArr []int) {
+func blink(stones map[int]int) (newStones map[int]int) {
 	cache := map[int][]int{}
 
-	subSlices := [][]int{}
-	for _, value := range input {
-		subSlices = append(subSlices, transformSingleValue(value, cache))
+	newStones = make(map[int]int)
+	for value, count := range stones {
+		transformSingleValue(newStones, value, count, cache)
 	}
 
-	return slices.Concat(subSlices...)
+	return newStones
 }
 
-func transformSingleValue(value int, cache map[int][]int) (result []int) {
-	if cache[value] != nil {
-		return cache[value]
+func transformSingleValue(newStones map[int]int, value int, count int, cache map[int][]int) {
+	if cache[value] == nil {
+		var tmpStones []int
+
+		str := fmt.Sprintf("%d", value)
+		switch {
+		case value == 0:
+			tmpStones = []int{1}
+		case len(str)%2 == 0:
+			val1, _ := strconv.Atoi(str[:len(str)/2])
+			val2, _ := strconv.Atoi(str[len(str)/2:])
+			tmpStones = []int{val1, val2}
+		default:
+			tmpStones = []int{value * 2024}
+		}
+
+		cache[value] = tmpStones
 	}
 
-	str := fmt.Sprintf("%d", value)
-	switch {
-	case value == 0:
-		result = []int{1}
-	case len(str)%2 == 0:
-		val1, _ := strconv.Atoi(str[:len(str)/2])
-		val2, _ := strconv.Atoi(str[len(str)/2:])
-		result = []int{val1, val2}
-	default:
-		result = []int{value * 2024}
+	for _, newStone := range cache[value] {
+		newStones[newStone] += count
 	}
-
-	cache[value] = result
-	return result
 }
