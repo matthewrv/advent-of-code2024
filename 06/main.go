aefd73079f1ded2cabf2f8ddea835ffcd43b66b8
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2024-12-16 11:27:57 +0300

    solve part 2 of day 6

--- 06/main.go
+++ 06/main.go
@@ -8,9 +8,10 @@ import (
 )
 
 func main() {
-	size, obstructionsMap, position, direction := readInput("input.txt")
-	visited := countVisitedPositions(size, obstructionsMap, position, direction)
-	fmt.Printf("Result: %d\n", visited)
+	size, obstructionsMap, start := readInput("input.txt")
+	// result := countVisitedPositions(size, obstructionsMap, start)
+	result := countCycledRoutes(size, obstructionsMap, start)
+	fmt.Printf("Result: %d\n", result)
 }
 
 // helper struct
@@ -24,9 +25,14 @@ func (v1 *vector) Sum(v2 *vector) vector {
 	return vector{v1.x + v2.x, v1.y + v2.y}
 }
 
+type Visit struct {
+	position  vector
+	direction vector
+}
+
 // read input
 
-func readInput(fileName string) (size vector, obstructionsMap map[vector]bool, position vector, direction vector) {
+func readInput(fileName string) (size vector, obstructionsMap map[vector]bool, start Visit) {
 	f, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
@@ -47,9 +53,8 @@ func readInput(fileName string) (size vector, obstructionsMap map[vector]bool, p
 			case '#':
 				obstructionsMap[vector{col, line}] = true
 			case '^':
-				position = vector{col, line}
 				// axis are assumed to increase in direction of increasing index of array
-				direction = vector{0, -1}
+				start = Visit{vector{col, line}, vector{0, -1}}
 			}
 		}
 
@@ -59,17 +64,86 @@ func readInput(fileName string) (size vector, obstructionsMap map[vector]bool, p
 
 	size.y = line + 1
 
-	return size, obstructionsMap, position, direction
+	return size, obstructionsMap, start
 }
 
 // part 1
 
-func countVisitedPositions(size vector, obstructionsMap map[vector]bool, position vector, direction vector) int {
+func countVisitedPositions(size vector, obstructionsMap map[vector]bool, start Visit) int {
 	visitedPositions := map[vector]bool{}
-	visitedPositions[position] = true
+	visitedPositions[start.position] = true
 
-	gridSizeX := size.x
-	gridSizeY := size.y
+	current := start
+	for {
+		current = getNextPosition(current, obstructionsMap)
+		if current.isOutOfMap(size) {
+			// guard left the map
+			break
+		}
+
+		visitedPositions[current.position] = true
+	}
+
+	return len(visitedPositions)
+}
+
+// part 2
+
+func countCycledRoutes(size vector, obstructionsMap map[vector]bool, start Visit) int {
+	possibleObstacles := make(map[vector]bool)
+
+	current := start
+	for {
+		newPosition := getNextPosition(current, obstructionsMap)
+		if newPosition.isOutOfMap(size) {
+			// guard left the map
+			break
+		}
+
+		// debug(newPosition.position, newPosition.direction)
+		obstacleExists := obstructionsMap[newPosition.position]
+		if !obstacleExists {
+			obstructionsMap[newPosition.position] = true
+			if isCycle(size, obstructionsMap, start) {
+				possibleObstacles[newPosition.position] = true
+			}
+			obstructionsMap[newPosition.position] = false
+		}
+
+		current = newPosition
+	}
+
+	return len(possibleObstacles)
+}
+
+func isCycle(size vector, obstructionsMap map[vector]bool, start Visit) bool {
+	visitedPositions := make(map[Visit]bool)
+	visitedPositions[start] = true
+
+	position := start
+	for {
+		newPosition := getNextPosition(position, obstructionsMap)
+		if newPosition.isOutOfMap(size) {
+			break
+		}
+
+		if visitedPositions[newPosition] {
+			return true
+		}
+
+		visitedPositions[newPosition] = true
+		position = newPosition
+	}
+
+	return false
+}
+
+func debug(position vector, direction vector) {
+	fmt.Printf("Current position: %v, current direction: %v\n", position, direction)
+}
+
+func getNextPosition(position Visit, obstructionsMap map[vector]bool) (newVisit Visit) {
+	newPosition := position.position.Sum(&position.direction)
 
 	rotationRules := map[vector]vector{
 		{0, -1}: {1, 0},
@@ -78,29 +152,24 @@ func countVisitedPositions(size vector, obstructionsMap map[vector]bool, positio
 		{-1, 0}: {0, -1},
 	}
 
-	for {
-		newPosition := position.Sum(&direction)
-		if newPosition.x < 0 || newPosition.y < 0 || newPosition.x >= gridSizeX || newPosition.y >= gridSizeY {
-			// guard left the map
-			break
-		}
-
-		if obstructionsMap[newPosition] {
-			// rotate guard
-			direction = rotationRules[direction]
-			// fmt.Println("Found obstacle, rotate.")
-			// debug(position, direction)
-		} else {
-			// move the guard
-			position = newPosition
-			visitedPositions[position] = true
-			// debug(position, direction)
-		}
+	if obstructionsMap[newPosition] {
+		// rotate guard
+		newVisit = Visit{position: position.position, direction: rotationRules[position.direction]}
+		// fmt.Println("Found obstacle, rotate.")
+		// debug(position, direction)
+	} else {
+		// move the guard
+		newVisit = Visit{position: newPosition, direction: position.direction}
+		// debug(position, direction)
 	}
 
-	return len(visitedPositions)
+	return newVisit
 }
 
-func debug(position vector, direction vector) {
-	fmt.Printf("Current position: %v, current direction: %v\n", position, direction)
+func (v *Visit) isOutOfMap(size vector) bool {
+	if v.position.x < 0 || v.position.y < 0 || v.position.x >= size.x || v.position.y >= size.y {
+		// guard left the map
+		return true
+	}
+	return false
 }
