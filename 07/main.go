fe8374495e4955a1fdd3f11039fddebf4e6d1267
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2024-12-17 15:09:18 +0300

    solve part 1 of day 7

--- 07/main.go
+++ 07/main.go
@@ -38,88 +38,28 @@ func solvePart1(fileName string) (result int) {
 		)
 		var (
 			elements []int
-			maxValue int
 		)
 		for _, token := range tokens {
 			num, _ := strconv.Atoi(token)
 			elements = append(elements, num)
-			if maxValue == 0 {
-				maxValue += num
-			} else if num == 1 {
-				maxValue += 1
-			} else {
-				maxValue *= num
-			}
 		}
 		idx++
 
-		// solve line
-
-		// edge cases
-		if maxValue < total {
-			fmt.Printf("Non-solvable line %d: %d - %v | %d\n", idx, total, elements, maxValue)
-			continue
-		}
-		if maxValue == total {
+		if bruteForce(total, elements[0], elements[1:]) {
 			result += total
-			continue
 		}
-
-		// general case
-		// stack := []Operation{}
-		// for _ = range elements {
-		// 	stack = append(stack, Product)
-		// }
-		// stack = stack[:len(stack)-1]
-		result += generalCase(total, elements[0], elements[1:])
-
 	}
 
 	return result
 }
 
-type Operation int
-
-const (
-	Sum Operation = iota
-	Product
-)
-
-func generalCase(total int, current int, elements []int) (closest int) {
-	val := elements[0]
-
+func bruteForce(total int, current int, elements []int) bool {
 	if len(elements) == 1 {
-		if current*val == total {
-			return total
+		if current+elements[0] == total || current*elements[0] == total {
+			return true
 		}
-		if current+val == total {
-			return total
-		}
-		return 0
+		return false
 	}
 
-	closest = generalCase(total, current*val, elements[1:])
-	if closest == total {
-		return total
-	}
-	closest = generalCase(total, current+val, elements[1:])
-	if closest == total {
-		return total
-	}
-
-	return 0
+	return bruteForce(total, current+elements[0], elements[1:]) || bruteForce(total, current*elements[0], elements[1:])
 }
-
-// func getMaxResult(current int, element int) int {
-// 	if element == 1 {
-// 		return current + element
-// 	}
-// 	return current * element
-// }
-
-// func getMinResult(current int, element int) int {
-// 	if element == 1 {
-// 		return current * element
-// 	}
-// 	return current + element
-// }
